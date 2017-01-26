704e274b75380c7108c69afc7297a448c7883b4c
Andrew Timokhin <tandser@gmail.com>
2017-01-26 14:49:46 +0700

    Add tests for datajpa meal repository

--- src/test/java/ru/tandser/solution/MealTestData.java
+++ src/test/java/ru/tandser/solution/MealTestData.java
@@ -1,16 +1,53 @@
 package ru.tandser.solution;
 
+import org.exolab.castor.mapping.Mapping;
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.XMLContext;
+import org.springframework.util.ResourceUtils;
 import ru.tandser.solution.domain.Meal;
 import ru.tandser.solution.util.Matcher;
+import ru.tandser.solution.util.Meals;
 
+import java.io.FileReader;
+import java.util.List;
 import java.util.Objects;
 
 public class MealTestData {
 
-    public static Meal meal;
+    public static List<Meal> meals;
+    public static Meal       newMeal;
+    public static Meal       duplicateMeal;
+    public static Meal       invalidDateTimeMeal;
+    public static Meal       invalidDescriptionMeal;
+    public static Meal       invalidCaloriesMeal;
 
     public static final Matcher<Meal> MEAL_MATCHER = new Matcher<>((expected, actual) ->
             expected == actual || (Objects.equals(expected.getDateTime(),    actual.getDateTime())    &&
                                    Objects.equals(expected.getDescription(), actual.getDescription()) &&
                                    Objects.equals(expected.getCalories(),    actual.getCalories())));
+
+    public static void loadMocks() throws Exception {
+        FileReader reader = new FileReader(ResourceUtils.getFile("classpath:mocks/meals.xml"));
+
+        Mapping mapping = new Mapping();
+        mapping.loadMapping(ResourceUtils.getURL("classpath:castor/mapping.xml"));
+
+        XMLContext xmlContext = new XMLContext();
+        xmlContext.addMapping(mapping);
+
+        Unmarshaller unmarshaller = xmlContext.createUnmarshaller();
+        unmarshaller.setClass(Meals.class);
+
+        Meals mocks = (Meals) unmarshaller.unmarshal(reader);
+
+        reader.close();
+
+        meals                  = mocks.getMeals().subList(0, 12);
+        newMeal                = mocks.getMeals().get(12);
+        duplicateMeal          = mocks.getMeals().get(13);
+        invalidDateTimeMeal    = mocks.getMeals().get(14);
+        invalidDescriptionMeal = mocks.getMeals().get(15);
+        invalidCaloriesMeal    = mocks.getMeals().get(16);
+    }
 }
--- src/test/java/ru/tandser/solution/repository/DataJpaMealRepositoryTest.java
+++ src/test/java/ru/tandser/solution/repository/DataJpaMealRepositoryTest.java
@@ -1,7 +1,12 @@
 package ru.tandser.solution.repository;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import ru.tandser.solution.MealTestData;
+
+import javax.validation.ConstraintViolationException;
 
 import static org.junit.Assert.*;
 import static ru.tandser.solution.MealTestData.*;
@@ -15,15 +20,56 @@ public class DataJpaMealRepositoryTest extends AbstractRepositoryTest {
         this.mealRepository = mealRepository;
     }
 
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        MealTestData.loadMocks();
+    }
+
     @Test
     public void testGet() throws Exception {
         assertNull(mealRepository.get(0, 2));
         assertNull(mealRepository.get(2, 0));
-        assertTrue(MEAL_MATCHER.equals(meal, mealRepository.get(1, 2)));
+        assertTrue(MEAL_MATCHER.equals(meals.get(0), mealRepository.get(1, 2)));
     }
 
     @Test
     public void testGetAll() throws Exception {
+        assertTrue(mealRepository.getAll(0).isEmpty());
+        assertTrue(MEAL_MATCHER.equals(meals, mealRepository.getAll(2)));
+    }
 
+    @Test
+    public void testRemove() throws Exception {
+        assertNull(mealRepository.remove(0, 2));
+        assertNull(mealRepository.remove(2, 0));
+
+        assertTrue(MEAL_MATCHER.equals(meals.get(0), mealRepository.remove(1, 2)));
+        assertTrue(MEAL_MATCHER.equals(meals.subList(1, meals.size()), mealRepository.getAll(2)));
+    }
+
+    @Test
+    public void testPut() throws Exception {
+        assertNull(mealRepository.put(newMeal, 0));
+        assertNull(newMeal.getId());
+
+        assertTrue(MEAL_MATCHER.equals(newMeal, mealRepository.put(newMeal, 2)));
+        assertTrue(MEAL_MATCHER.equals(newMeal, mealRepository.get(newMeal.getId(), 2)));
+
+        newMeal.setCalories(500);
+
+        assertTrue(MEAL_MATCHER.equals(newMeal, mealRepository.put(newMeal, 2)));
+        assertTrue(MEAL_MATCHER.equals(newMeal, mealRepository.get(newMeal.getId(), 2)));
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testPutDuplicateUserIdDateTime() throws Exception {
+        mealRepository.put(duplicateMeal, 2);
+    }
+
+    @Test
+    public void testValidation() throws Exception {
+        validateRootCause(() -> mealRepository.put(invalidDateTimeMeal, 2),    ConstraintViolationException.class);
+        validateRootCause(() -> mealRepository.put(invalidDescriptionMeal, 2), ConstraintViolationException.class);
+        validateRootCause(() -> mealRepository.put(invalidCaloriesMeal, 2),    ConstraintViolationException.class);
     }
 }
