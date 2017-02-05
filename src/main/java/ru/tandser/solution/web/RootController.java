cc9bd5173d44a0051f3064293580ae190f5aaae3
Andrew Timokhin <tandser@gmail.com>
2017-02-05 17:02:44 +0700

    Fix

--- src/main/java/ru/tandser/solution/web/RootController.java
+++ src/main/java/ru/tandser/solution/web/RootController.java
@@ -1,29 +1,14 @@
 package ru.tandser.solution.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.tandser.solution.service.UserService;
 
 @Controller
 public class RootController {
 
-    private UserService userService;
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping("/")
     public String root() {
-        return "redirect:users";
-    }
-
-    @GetMapping("/meals")
-    public String meals() {
-        return "meals";
+        return "redirect:meals";
     }
 
     @GetMapping("/login")
@@ -31,9 +16,13 @@ public class RootController {
         return "login";
     }
 
+    @GetMapping("/meals")
+    public String meals() {
+        return "meals";
+    }
+
     @GetMapping("/users")
-    public String users(Model model) {
-        model.addAttribute("users", userService.getAll());
+    public String users() {
         return "users";
     }
 }
