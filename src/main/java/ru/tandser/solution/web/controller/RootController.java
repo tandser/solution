cf94fdd39fdf2c371c75f5ffcf81f5a1a57069d4
Andrew Timokhin <tandser@gmail.com>
2017-02-18 20:12:17 +0700

    Move RootController

--- src/main/java/ru/tandser/solution/web/controller/RootController.java
+++ src/main/java/ru/tandser/solution/web/controller/RootController.java
@@ -1,4 +1,4 @@
-package ru.tandser.solution.web;
+package ru.tandser.solution.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
